c44a1c305431a058c116c65b150b86a964a7b8ba
Merve Erdol <merveuygunerdol@gmail.com>
2022-12-06 15:56:02 +0300

    Create dist folder

---
+++ dist/soft-delete-plugin.d.ts
@@ -0,0 +1,4 @@
+import mongoose from 'mongoose';
+export declare const softDeletePlugin: (schema: mongoose.Schema<any, mongoose.Model<any, any, any, any, any>, {}, {}, {}, {}, "type", {
+    [x: string]: any;
+}>) => void;
